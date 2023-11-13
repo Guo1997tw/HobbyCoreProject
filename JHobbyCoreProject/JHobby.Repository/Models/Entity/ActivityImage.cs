938ad886b2dc5e2851746c7ebd13750d25cf9e04
Kenny Guo <Guo1997tw@gmail.com>
2023-11-13 20:39:15 +0800

    [Revise] - 重新匯入DB Context

--- JHobbyCoreProject/JHobby.Repository/Models/Entity/ActivityImage.cs
+++ JHobbyCoreProject/JHobby.Repository/Models/Entity/ActivityImage.cs
@@ -7,11 +7,13 @@ public partial class ActivityImage
 {
     public int Id { get; set; }
 
-    public string ActivityId { get; set; } = null!;
+    public int ActivityId { get; set; }
 
     public string ActivityImage1 { get; set; } = null!;
 
     public bool IsCover { get; set; }
 
     public DateTime UploadTime { get; set; }
+
+    public virtual Activity Activity { get; set; } = null!;
 }
