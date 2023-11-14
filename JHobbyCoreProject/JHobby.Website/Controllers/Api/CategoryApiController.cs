4cee28facd72f63e602dfeabb1a22c007675491a
Kenny Guo <Guo1997tw@gmail.com>
2023-11-14 19:00:33 +0800

    [Revise] - 增加AJAX

--- JHobbyCoreProject/JHobby.Website/Controllers/Api/CategoryApiController.cs
+++ JHobbyCoreProject/JHobby.Website/Controllers/Api/CategoryApiController.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace JHobby.Website.Controllers.Api
 {
-	[Route("api/[controller]")]
+	[Route("api/[controller]/[action]")]
 	[ApiController]
 	public class CategoryApiController : ControllerBase
 	{
