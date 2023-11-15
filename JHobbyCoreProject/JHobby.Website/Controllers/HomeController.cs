ce13012b9083cf606b453a08774b2f16b0602600
Henry <henry00614@hotmail.com>
2023-11-15 23:56:32 +0800

    [Revise] - 修改中繼頁面內容
    Signed-off-by: Henry <henry00614@hotmail.com>

--- JHobbyCoreProject/JHobby.Website/Controllers/HomeController.cs
+++ JHobbyCoreProject/JHobby.Website/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace JHobby.Website.Controllers
             return View();
         }
 
+        public IActionResult Middle()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
