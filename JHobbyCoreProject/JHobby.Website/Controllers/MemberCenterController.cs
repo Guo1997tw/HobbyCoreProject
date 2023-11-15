2f2269bca5ffeb0d56d86822d995f1b44138f70c
ArvinSu <it.jing.yin@gmail.com>
2023-11-16 00:58:43 +0800

    [New] 參團者頁面

---
+++ JHobbyCoreProject/JHobby.Website/Controllers/MemberCenterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JHobby.Website.Controllers
+{
+    public class MemberCenterController : Controller
+    {
+        
+        // MemberCenter/launchATeam
+        /// <summary>
+        /// TODO : 開團紀錄
+        /// </summary>
+        public IActionResult launchATeam()
+        {
+            return View();
+        }
+    }
+}
