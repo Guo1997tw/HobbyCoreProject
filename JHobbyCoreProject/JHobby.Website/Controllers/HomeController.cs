e83da7a5175d163306c3fc06a94a0fce8bd8b486
shinji <tq0514@yahoo.com.tw>
2023-11-16 16:05:03 +0800

    新增  =>  [New] - 在HomeController新建leaderEdit頁面

--- JHobbyCoreProject/JHobby.Website/Controllers/HomeController.cs
+++ JHobbyCoreProject/JHobby.Website/Controllers/HomeController.cs
@@ -23,7 +23,12 @@ namespace JHobby.Website.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult leaderEdit()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
