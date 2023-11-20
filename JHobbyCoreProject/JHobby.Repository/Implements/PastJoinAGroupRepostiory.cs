d71b12d5053f06a483401fbda078d30d8ea50010
ArvinSu <it.jing.yin@gmail.com>
2023-11-20 12:58:27 +0800

    [New] - 建立PastJoinAGroupRepostiory [New] - 建立IPastJoinAGroupRepostiory [New] - 建立PastJoinAGroupDto

---
+++ JHobbyCoreProject/JHobby.Repository/Implements/PastJoinAGroupRepostiory.cs
@@ -0,0 +1,46 @@
+﻿using JHobby.Repository.Interfaces;
+using JHobby.Repository.Models.Dto;
+using JHobby.Repository.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHobby.Repository.Implements
+{
+    internal class PastJoinAGroupRepostiory : IPastJoinAGroupRepostiory
+    {
+        private readonly JhobbyContext _JhobbyContext;
+
+        public PastJoinAGroupRepostiory(JhobbyContext jhobbyContext)
+        {
+            
+            _JhobbyContext = jhobbyContext;
+        }
+
+
+        public IEnumerable<PastJoinAGroupDto> GetPastJoinAGroupAll()
+        {
+            var queryResult = _JhobbyContext.Activities.Join(
+                _JhobbyContext.Members,
+                a => a.MemberId,
+                m => m.MemberId,
+                (a,m) => new PastJoinAGroupDto
+                {
+                    ActivityId = a.ActivityId,
+                    MemberId = a.MemberId,
+                    ActivityName = a.ActivityName,
+                    ActivityStatus = a.ActivityStatus,
+                    ActivityCity = a.ActivityCity,
+                    CurrentPeople = a.CurrentPeople,
+                    StartTime = a.StartTime,
+                    NickName = m.NickName
+                }
+                );
+
+            return queryResult;
+        }
+    }
+
+}
---
+++ JHobbyCoreProject/JHobby.Repository/Interfaces/IPastJoinAGroupRepostiory.cs
@@ -0,0 +1,14 @@
+﻿using JHobby.Repository.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHobby.Repository.Interfaces
+{
+    public interface IPastJoinAGroupRepostiory
+    {
+        public IEnumerable<PastJoinAGroupDto> GetPastJoinAGroupAll();
+    }
+}
