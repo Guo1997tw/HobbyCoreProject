e43bf9524a1a92f5026fefd48453590a4c385528
Kenny Guo <Guo1997tw@gmail.com>
2023-12-04 22:29:33 +0800

    [Revise] - 調整團主建立、編輯表單路由、View名稱

---
+++ JHobbyCoreProject/JHobby.Website/Controllers/ActivityLeaderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JHobby.Website.Controllers
+{
+	public class ActivityLeaderController : Controller
+	{
+		public IActionResult LeaderBuild()
+		{
+			return View();
+		}
+        public IActionResult LeaderEdit()
+        {
+            return View();
+        }
+    }
+}
