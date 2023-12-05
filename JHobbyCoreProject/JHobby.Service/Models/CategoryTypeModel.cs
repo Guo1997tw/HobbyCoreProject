b8e782dc6a29a32c5f00003d037bad1cbf10df9b
Kenny Guo <Guo1997tw@gmail.com>
2023-12-06 03:11:05 +0800

    [New] - 新增種類項目Model

---
+++ JHobbyCoreProject/JHobby.Service/Models/CategoryTypeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHobby.Service.Models
+{
+    public class CategoryTypeModel
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = null!;
+
+        public IEnumerable<CategoryDetailModel> CategoryDetails { get; set; } = new List<CategoryDetailModel>();
+    }
+}
