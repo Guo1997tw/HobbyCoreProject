67e8f59973a1e3ba6221a060b80019a8583b94d0
Leo <leo27961541@gmail.com>
2023-12-06 15:21:38 +0800

    [Revise] -加入身分驗證及照片格式(目前差不多都完成,待最後測驗)

--- JHobbyCoreProject/JHobby.Website/Controllers/ReviewController.cs
+++ JHobbyCoreProject/JHobby.Website/Controllers/ReviewController.cs
@@ -1,17 +1,23 @@
-﻿using JHobby.Website.Models.ViewModels;
+﻿using JHobby.Service.Interfaces;
+using JHobby.Website.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace JHobby.Website.Controllers
 {
 	public class ReviewController : Controller
 	{
-
+		private readonly IUserAuthenticationService _userAuthenticationService;
+		public ReviewController(IUserAuthenticationService userAuthenticationService)
+		{
+			_userAuthenticationService = userAuthenticationService;
+		}
 		public IActionResult Apply(int id)
 		{
 			ViewData["Title"] = "報名審核";
+
             return View(new ReviewIdViewModel
             {
-                LeaderId = id
+                MemberId =_userAuthenticationService.GetUserId(),
             });
             
 		}
--- JHobbyCoreProject/JHobby.Website/Models/ViewModels/ReviewIdViewModel.cs
+++ JHobbyCoreProject/JHobby.Website/Models/ViewModels/ReviewIdViewModel.cs
@@ -2,10 +2,6 @@
 {
     public class ReviewIdViewModel
     {
-        public int LeaderId { get; set; }  //團主Id
-
-        public int ApplicantId { get; set; } //申請者Id
-        public int ActivityId { get; set; }
         public int MemberId { get; set; }
     }
 }
