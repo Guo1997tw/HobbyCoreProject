94fefa4ddbde6e8b4664cf8730170c37be708b2d
Kenny Guo <Guo1997tw@gmail.com>
2023-12-07 15:23:40 +0800

    [Revise] - 修改團主建立路由

--- JHobbyCoreProject/JHobby.Website/Controllers/ActivityLeaderController.cs
+++ JHobbyCoreProject/JHobby.Website/Controllers/ActivityLeaderController.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using JHobby.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JHobby.Website.Controllers
 {
 	public class ActivityLeaderController : Controller
 	{
+		private readonly IUserAuthenticationService _userAuthenticationService;
+
+		public ActivityLeaderController(IUserAuthenticationService userAuthenticationService)
+		{
+			_userAuthenticationService = userAuthenticationService;		
+		}
+
 		public IActionResult LeaderBuild()
 		{
+			try
+			{
+                ViewBag.buildMemberId = _userAuthenticationService.GetUserId();
+            }
+			catch (Exception)
+			{
+
+				throw new Exception("Error!");
+            }
+
 			return View();
 		}
-        public IActionResult LeaderEdit()
+		public IActionResult LeaderEdit()
         {
             return View();
         }
