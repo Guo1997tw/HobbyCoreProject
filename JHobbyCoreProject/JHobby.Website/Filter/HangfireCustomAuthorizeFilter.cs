ca79289703d4e84f8806d796303c0705c9129622
Kenny Guo <Guo1997tw@gmail.com>
2023-12-12 16:55:00 +0800

    [New] - 增加Hangfire身份驗證

---
+++ JHobbyCoreProject/JHobby.Website/Filter/HangfireCustomAuthorizeFilter.cs
@@ -0,0 +1,13 @@
+﻿using Hangfire.Dashboard;
+using System.Diagnostics.CodeAnalysis;
+
+namespace JHobby.Website.Filter
+{
+    public class HangfireCustomAuthorizeFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
