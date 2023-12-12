d7927ac1c75470b533563e00293b072ae646aaa4
Kenny Guo <Guo1997tw@gmail.com>
2023-12-12 14:10:42 +0800

    [Revise] - Home/newcomer 遷移至 Introduce/Newcomer

---
+++ JHobbyCoreProject/JHobby.Website/Controllers/Introduce.cs
@@ -0,0 +1,31 @@
+﻿using JHobby.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JHobby.Website.Controllers
+{
+    public class Introduce : Controller
+    {
+        private readonly IUserAuthenticationService _userAuthenticationService;
+
+        public Introduce(IUserAuthenticationService userAuthenticationService)
+        {
+            _userAuthenticationService = userAuthenticationService;
+        }
+
+        public IActionResult Newcomer()
+        {
+            ViewData["Title"] = "新手上路";
+            try
+            {
+                ViewBag.verifyMemberId = _userAuthenticationService.GetUserId();
+            }
+            catch (Exception)
+            {
+
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
